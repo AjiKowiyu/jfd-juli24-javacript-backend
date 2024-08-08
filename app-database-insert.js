4fa25a05979f048f42f4ca694d44974fc8e2c5b8
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-08-08 19:42:46 +0700

    nodejs mysql insert & update

---
+++ app-database-insert.js
@@ -0,0 +1,32 @@
+const http  = require('http') // modul bawaan dari node.js
+const mysql = require('mysql2') // modul dari node_modules
+
+// konfigurasi database mysql yg ingin digunakan
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'jfd_belajar_database',
+})
+
+// menyambungkan atau membuka koneksi
+db.connect()
+
+let sql = 
+`INSERT INTO 
+karyawan (nama, gender, alamat, nip) 
+VALUES ('jokowi', 'L', 'Solo', '007')`
+
+// memasukkan/menambahkan data dari mysql
+db.query(sql, function(error, hasil) {
+    if (error) {
+        console.log(error)
+    } else {
+        // console.log(hasil)
+        if (hasil.affectedRows > 0) {
+            console.log('berhasil insert data karyawan')
+        }
+    }
+})
+
+db.end()
