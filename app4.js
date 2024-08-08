e68bce5047411948b26ef81100636ac860e38dd9
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-08-08 22:00:55 +0700

    query string di url

--- app4.js
+++ app4.js
@@ -1,5 +1,7 @@
 const http  = require('http')
 const fs    = require('fs')
+const URL   = require('url')
+const qs    = require('querystring')
 const mysql = require('mysql2')
 
 const db = mysql.createConnection({
@@ -14,6 +16,24 @@ db.connect()
 
 
 let server = http.createServer( function(request, respon) {
+
+    // console.log( qs.parse(URL.parse(request.url).query) )
+    let qstring = qs.parse(URL.parse(request.url).query)
+    console.log(qstring.nama)
+    if (qstring.nama) {
+        db.query(`SELECT * FROM karyawan WHERE nama = ?`,[qstring.nama] , function(error, hasil) {
+            if (error) {
+                console.log(error);
+            } else {
+                return respon.write(
+                    `<pre>
+                    ${JSON.stringify(hasil)}
+                    </pre>`
+                )
+            }
+        })
+    }
+
     if (request.url == '/') {
         respon.writeHead(200, {'Content-type': 'text/html'})
         fs.createReadStream('./view/beranda.html').pipe(respon)
