f88f0a24acdc837c18aeb46f9b31ac6465759353
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-08-09 21:59:17 +0700

    nodejs express

--- app-express.js
+++ app-express.js
@@ -3,13 +3,32 @@ const app       = express()
 const port      = 3000
 
 
+app.set('view engine','ejs')    //setting penggunaan template engine untuk express
+app.set('views', './view-ejs')  //setting penggunaan folder untuk menyimpan file .ejs
+
+
 app.get('/', function(req,res) {
     res.send('<h1>Hello World !!</h1>')
 })
 
-//route -> rute
+
 app.get('/hubungi', function(req,res) {
-    res.send('<h1>Silakan Wa: 081293260970</h1>')
+    let data = {
+        nomor_wa: '081293260970',
+        email: 'ajikowiyu@gmail.com',
+        linkedin: '@ajikowiyu',
+    }
+    res.render('hubungi', data)
+})
+
+
+app.get('/profil', function(req,res) {
+    let data = {
+        jabatan: 'Senior Manager',
+        gender: 'Laki',
+        gaji: 7000000,
+    }
+    res.render('profil-developer', data)
 })
 
 
