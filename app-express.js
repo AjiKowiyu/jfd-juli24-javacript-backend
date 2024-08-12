0094e739bdaddc86a89d74894cf2c40427c2be2c
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-08-12 21:50:30 +0700

    membuat menu detail karyawan

--- app-express.js
+++ app-express.js
@@ -66,6 +66,34 @@ app.get('/karyawan', async function(req,res) {
 })
 
 
+
+app.get('/karyawan/detail/:id_karyawan', async function(req,res) {
+
+    // ambil id yang dikirim via url
+    let idk = req.params.id_karyawan
+
+    // setelah itu kirim ke proses request data mysql
+    let dataview = {
+        pegawai: await get_satuKaryawan(idk),
+    }
+    res.render('karyawan/detail', dataview)
+})
+
+
+function get_satuKaryawan(idk) {
+    return new Promise( (resolve,reject)=>{
+        db.query("SELECT * FROM karyawan WHERE id = ?", [idk], function(errorSql, hasil) {
+            if (errorSql) {
+                reject(errorSql)
+            } else {
+                resolve(hasil)
+            }
+        })
+    })
+}
+
+
+
 app.listen(port, function() {
     console.log('Server sudah siap, buka http://localhost:'+port)
 })
