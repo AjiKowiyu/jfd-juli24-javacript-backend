dc8c19a878b2448e834d3aac8d57ad6742e37684
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-08-13 20:13:31 +0700

    hapus karyawan

--- app-express.js
+++ app-express.js
@@ -103,6 +103,39 @@ function get_satuKaryawan(idk) {
 }
 
 
+app.get('/karyawan/hapus/:id_karyawan', async function(req,res) {
+    // ambil id yang dikirim via url
+    let idk = req.params.id_karyawan
+
+    // proses hapus data
+    try {
+        let hapus = await hapus_satuKaryawan(idk)
+        if (hapus.affectedRows > 0) {
+            res.redirect('/karyawan')
+        }
+    } catch (error) {
+        throw error
+    }
+})
+
+
+function hapus_satuKaryawan(idk) {
+    let sql = 
+    `DELETE FROM karyawan
+    WHERE id = ?`;
+
+    return new Promise( (resolve,reject)=>{
+        db.query(sql, [idk], function(errorSql, hasil) {
+            if (errorSql) {
+                reject(errorSql)
+            } else {
+                resolve(hasil)
+            }
+        })
+    })
+}
+
+
 
 app.listen(port, function() {
     console.log('Server sudah siap, buka http://localhost:'+port)
