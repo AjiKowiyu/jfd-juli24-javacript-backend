3c908341aaf24af8a3852633c79dd47c1a6db082
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-08-15 21:59:56 +0700

    proses update data karyawan

--- app-express.js
+++ app-express.js
@@ -224,6 +224,42 @@ app.get('/karyawan/edit/:id_karyawan', async function(req,res) {
 })
 
 
+app.post('/karyawan/proses-update/:id_karyawan', async function(req,res) {
+    let idk = req.params.id_karyawan
+    try {
+        let update = await update_karyawan(req, idk)
+        if (update.affectedRows > 0) {
+            res.redirect('/karyawan')
+        }
+    } catch (error) {
+        throw error
+    }
+})
+
+
+function update_karyawan(req, idk) {
+    let data = {
+        nama            : req.body.form_nama_lengkap,
+        gender          : req.body.form_gender,
+        alamat          : req.body.form_alamat,
+        nip             : req.body.form_nip,
+        departemen_id   : req.body.form_departemen,
+        agama_id        : req.body.form_agama
+    }
+    let sql = `UPDATE karyawan SET ? WHERE id = ?`;
+
+    return new Promise( (resolve,reject)=>{
+        db.query(sql, [data, idk], function(errorSql, hasil) {
+            if (errorSql) {
+                reject(errorSql)
+            } else {
+                resolve(hasil)
+            }
+        })
+    })
+}
+
+
 
 app.listen(port, function() {
     console.log('Server sudah siap, buka http://localhost:'+port)
